c86db13833a999aef569e0a783e92722c1f89fcf
Thomas Rohloff <v10lator@myway.de>
2020-04-26 04:06:09 +0200

    Make error surface even smaller...
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/input.c
+++ src/input.c
@@ -11,14 +11,11 @@
 
 #include <vpad/input.h>
 #include <coreinit/memdefaultheap.h>
-#include <coreinit/memory.h>
 #include <coreinit/thread.h>
 #include <gui/memory.h>
 #include <whb/log.h>
 
 VPADStatus vpad;
-Swkbd_CreateArg createArg;
-FSClient *swkbdCli = NULL;
 bool kbdVisible;
 
 bool SWKBD_Show()
@@ -28,38 +25,7 @@ bool SWKBD_Show()
 		WHBLogPrintf("...while visible!!!");
 	
 	// Show the keyboard
-	Swkbd_AppearArg appearArg;
-	
-	OSBlockSet(&appearArg.keyboardArg.configArg, 0, sizeof(Swkbd_ConfigArg));
-	
-	appearArg.keyboardArg.configArg.languageType = Swkbd_LanguageType__English;
-	appearArg.keyboardArg.configArg.unk_0x04 = 4;
-	appearArg.keyboardArg.configArg.unk_0x08 = 0;
-	appearArg.keyboardArg.configArg.unk_0x0C = 0x7FFFF;
-	appearArg.keyboardArg.configArg.unk_0x10 = 19;
-	appearArg.keyboardArg.configArg.unk_0x14 = -1;
-	appearArg.keyboardArg.configArg.unk_0x9C = 1;
-	appearArg.keyboardArg.configArg.unk_0xA4 = -1;
-	
-	appearArg.keyboardArg.receiverArg.unk_0x00 = 0;
-	appearArg.keyboardArg.receiverArg.unk_0x04 = 0;
-	appearArg.keyboardArg.receiverArg.unk_0x08 = 0;
-	appearArg.keyboardArg.receiverArg.unk_0x0C = 0;
-	appearArg.keyboardArg.receiverArg.unk_0x10 = 0;
-	appearArg.keyboardArg.receiverArg.unk_0x14 = 2;
-	
-	appearArg.inputFormArg.unk_0x00 = 1;
-	appearArg.inputFormArg.unk_0x04 = -1;
-	appearArg.inputFormArg.unk_0x08 = (uint32_t)NULL;
-	appearArg.inputFormArg.unk_0x0C = (uint32_t)NULL;
-	appearArg.inputFormArg.unk_0x14 = 0;
-	appearArg.inputFormArg.unk_0x18 = 0x00008000;
-	appearArg.inputFormArg.unk_0x1C = true;
-	appearArg.inputFormArg.unk_0x1D = true;
-	appearArg.inputFormArg.unk_0x1E = true;
-	appearArg.inputFormArg.maxTextLength = -1;
-	
-	kbdVisible = Swkbd_AppearInputForm(appearArg);
+	kbdVisible = Swkbd_AppearInputForm();
 	WHBLogPrintf("Swkbd_AppearInputForm(): %s", kbdVisible ? "true" : "false");
 	if(!kbdVisible)
 		return false;
@@ -81,13 +47,7 @@ void SWKBD_Hide()
 
 void SWKBD_Render()
 {
-	Swkbd_ControllerInfo controllerInfo;
-	controllerInfo.vpad = &vpad;
-	controllerInfo.kpad[0] = NULL;
-	controllerInfo.kpad[1] = NULL;
-	controllerInfo.kpad[2] = NULL;
-	controllerInfo.kpad[3] = NULL;
-	Swkbd_Calc(controllerInfo);
+	Swkbd_Calc(vpad);
 
 	if (Swkbd_IsNeedCalcSubThreadFont())
 		Swkbd_CalcSubThreadFont();
@@ -98,40 +58,13 @@ void SWKBD_Render()
 bool SWKBD_Init()
 {
 	WHBLogPrintf("SWKBD_Init()");
-	swkbdCli = MEMAllocFromDefaultHeap(sizeof(FSClient));
-	createArg.workMemory = MEMAllocFromDefaultHeap(Swkbd_GetWorkMemorySize(0));
-	if(swkbdCli == NULL || createArg.workMemory == NULL)
-	{
-		WHBLogPrintf("SWKBD: Can't allocate memory!");
-		return false;
-	}
-	
-	FSAddClient(swkbdCli, 0);
-	
-	createArg.regionType = Swkbd_RegionType__Europe;
-	createArg.unk_0x08 = 0;
-	createArg.fsClient = swkbdCli;
-	
-	return Swkbd_Create(createArg);
+	return Swkbd_Create();
 }
 
 void SWKBD_Shutdown()
 {
 	WHBLogPrintf("SWKBD_Shutdown()");
-	
-	if(createArg.workMemory != NULL)
-	{
-		Swkbd_Destroy();
-		MEMFreeToDefaultHeap(createArg.workMemory);
-		createArg.workMemory = NULL;
-	}
-	
-	if(swkbdCli != NULL)
-	{
-		FSDelClient(swkbdCli, 0);
-		MEMFreeToDefaultHeap(swkbdCli);
-		swkbdCli = NULL;
-	}
+	Swkbd_Destroy();
 }
 
 void readInput()
--- src/swkbd_wrapper.cpp
+++ src/swkbd_wrapper.cpp
@@ -8,23 +8,72 @@
 #include <stdint.h>
 #include <cstring>
 
+#include <coreinit/filesystem.h>
 #include <coreinit/memdefaultheap.h>
+#include <coreinit/memory.h>
+#include <whb/log.h>
 
 #include "swkbd_wrapper.h"
+
+nn::swkbd::CreateArg createArg;
+FSClient *swkbdCli = NULL;
 
 uint32_t Swkbd_GetWorkMemorySize(uint32_t unk)
 {
 	return nn::swkbd::GetWorkMemorySize(unk);
 }
 
-bool Swkbd_AppearInputForm(const Swkbd_AppearArg args)
+bool Swkbd_AppearInputForm()
 {
-	return nn::swkbd::AppearInputForm(*(nn::swkbd::AppearArg*)&args);
+	nn::swkbd::AppearArg appearArg;
+	
+	OSBlockSet(&appearArg.keyboardArg.configArg, 0, sizeof(nn::swkbd::AppearArg));
+	
+	appearArg.keyboardArg.configArg.languageType = nn::swkbd::LanguageType::English;
+	appearArg.keyboardArg.configArg.unk_0x04 = 4;
+	appearArg.keyboardArg.configArg.unk_0x08 = 0;
+	appearArg.keyboardArg.configArg.unk_0x0C = 0x7FFFF;
+	appearArg.keyboardArg.configArg.unk_0x10 = 19;
+	appearArg.keyboardArg.configArg.unk_0x14 = -1;
+	appearArg.keyboardArg.configArg.unk_0x9C = 1;
+	appearArg.keyboardArg.configArg.unk_0xA4 = -1;
+	
+	appearArg.keyboardArg.receiverArg.unk_0x00 = 0;
+	appearArg.keyboardArg.receiverArg.unk_0x04 = 0;
+	appearArg.keyboardArg.receiverArg.unk_0x08 = 0;
+	appearArg.keyboardArg.receiverArg.unk_0x0C = 0;
+	appearArg.keyboardArg.receiverArg.unk_0x10 = 0;
+	appearArg.keyboardArg.receiverArg.unk_0x14 = 2;
+	
+	appearArg.inputFormArg.unk_0x00 = 1;
+	appearArg.inputFormArg.unk_0x04 = -1;
+	appearArg.inputFormArg.unk_0x08 = (uint32_t)nullptr;
+	appearArg.inputFormArg.unk_0x0C = (uint32_t)nullptr;
+	appearArg.inputFormArg.unk_0x14 = 0;
+	appearArg.inputFormArg.unk_0x18 = 0x00008000;
+	appearArg.inputFormArg.unk_0x1C = true;
+	appearArg.inputFormArg.unk_0x1D = true;
+	appearArg.inputFormArg.unk_0x1E = true;
+	appearArg.inputFormArg.maxTextLength = -1;
+	return nn::swkbd::AppearInputForm(appearArg);
 }
 
-bool Swkbd_Create(const Swkbd_CreateArg args)
+bool Swkbd_Create()
 {
-	return nn::swkbd::Create(*(nn::swkbd::CreateArg*)&args);
+	swkbdCli = (FSClient *)MEMAllocFromDefaultHeap(sizeof(FSClient));
+	createArg.workMemory = MEMAllocFromDefaultHeap(Swkbd_GetWorkMemorySize(0));
+	if(swkbdCli == NULL || createArg.workMemory == NULL)
+	{
+		WHBLogPrintf("SWKBD: Can't allocate memory!");
+		return false;
+	}
+	
+	FSAddClient(swkbdCli, 0);
+	
+	createArg.regionType = nn::swkbd::RegionType::Europe;
+	createArg.unk_0x08 = 0;
+	createArg.fsClient = swkbdCli;
+	return nn::swkbd::Create(createArg);
 }
 
 void Swkbd_SetEnableOkButton(bool enable)
@@ -32,9 +81,15 @@ void Swkbd_SetEnableOkButton(bool enable)
 	nn::swkbd::SetEnableOkButton(enable);
 }
 
-void Swkbd_Calc(const Swkbd_ControllerInfo controllerInfo)
+void Swkbd_Calc(VPADStatus vpad)
 {
-	nn::swkbd::Calc(*(nn::swkbd::ControllerInfo*)&controllerInfo);
+	nn::swkbd::ControllerInfo controllerInfo;
+	controllerInfo.vpad = &vpad;
+	controllerInfo.kpad[0] = nullptr;
+	controllerInfo.kpad[1] = nullptr;
+	controllerInfo.kpad[2] = nullptr;
+	controllerInfo.kpad[3] = nullptr;
+	nn::swkbd::Calc(controllerInfo);
 }
 
 bool Swkbd_IsNeedCalcSubThreadFont()
@@ -65,4 +120,16 @@ bool Swkbd_DisappearInputForm()
 void Swkbd_Destroy()
 {
 	nn::swkbd::Destroy();
+	if(createArg.workMemory != NULL)
+	{
+		MEMFreeToDefaultHeap(createArg.workMemory);
+		createArg.workMemory = NULL;
+	}
+	
+	if(swkbdCli != NULL)
+	{
+		FSDelClient(swkbdCli, 0);
+		MEMFreeToDefaultHeap(swkbdCli);
+		swkbdCli = NULL;
+	}
 }
